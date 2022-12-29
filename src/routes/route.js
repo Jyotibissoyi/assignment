0cb80c550e05c769db2e5fd53fd33f8de6093307
Priyanka9120 <priyanka912066@gmail.com>
2022-12-30 02:43:45 +0530

    final push

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,26 +1,30 @@
 const express =require("express")
-const mongoose = require("mongoose")
 const router =express.Router();
+//=================================================================Importing modules================================================================>
 const { createUser, loginUser, getUser, updateUser } = require("../controller/userController")
 const{createProduct,filterProduct,productsById,updateProducts,deleteProductById} =require("../controller/productController")
 const{authenticate,authorize} =require("../middleware/middleware")
 const{createCart,updateCart,deleteCart,getCart} =require("../controller/cartController")
 const{createOrder,updateOrder}= require("../controller/orderController")
 
+
+//=============================User Feature===========================================//
 router.post("/register",createUser)
 router.post("/login",loginUser)
 router.get("/user/:userId/profile",authenticate,getUser)
 router.put("/user/:userId/profile",authenticate,authorize,updateUser)
+//=============================Product Feature===========================================//
 router.post("/products",createProduct)
 router.get("/products",filterProduct)
 router.get("/products/:productId",productsById)
 router.put("/products/:productId",updateProducts)
 router.delete("/products/:productId",deleteProductById)
-
+//=============================Cart Feature===========================================//
 router.post("/users/:userId/cart",authenticate,authorize,createCart)
 router.put("/users/:userId/cart",authenticate,authorize,updateCart)
 router.get("/users/:userId/cart",authenticate,authorize,getCart)
 router.delete("/users/:userId/cart",authenticate,authorize,deleteCart)
+//=============================Order Feature===========================================//
 router.post("/users/:userId/orders",authenticate,authorize,createOrder)
 router.put("/users/:userId/orders",authenticate,authorize,updateOrder)
 
